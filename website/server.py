28ec8ef966e4c6f9e3c9e24c2dc191c9094a7cfd
sooham <rafizsooham@gmail.com>
2018-03-03 12:36:07 -0800

    Added get to recipe API

--- website/server.py
+++ website/server.py
@@ -1,19 +1,28 @@
 from __future__ import print_function
+
+import pdb
+import json
+import requests
+import urllib
+
 from flask import Flask
 from flask import render_template
 from flask import request
 
 app = Flask(__name__)
 
+API_URL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract"
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/recipeLink", methods=['POST'])
+@app.route("/recipeLink")
 def process_recipe():
-    assert request.method == 'POST'
-
-    return "recieved recipe link: %s" % (request.form['recipeLink'])
+    recipe_url = urllib.quote(request.args.get('recipeLink'), safe='')
+    headers = {"X-Mashape-Key" : "e3LHtLGp7Smshyw7E0qaeCjHiuR3p1DczjljsnvW3aq1XYuWyz"}
+    r = requests.get(API_URL+"?forceExtraction=false&url="+recipe_url, headers=headers)
+    return str(r.json())
 
 @app.route("/login", methods=['POST'])
 def login():
